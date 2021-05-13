abcacceadc2f0ff3b3eaa9e6b129c5b54cedb374
pekkasagner <pekka.sagner@yahoo.de>
2021-05-13 08:36:30 +0200

    Update color_palette.R

--- color_palette.R
+++ color_palette.R
@@ -1,13 +1,13 @@
 library(tidyverse)
 
-test <- tibble(x = 1:9, y = 1:9)
+test <- tibble(x = 1:10, y = 1:10)
 
-woodworkR_palette_names <- c("ebony", "wenge", "walnut", "redwood", 
-                             "whiteoak", "cedar", "pine", "mahogany", 
-                             "beech", "teak")
-woodworkR_palette <- c("#2c3227", "#645452", "#6c3f19", "#5F3730", 
-                       "#ce9f6f", "#a1655b", "#deccba", "#c04000", 
-                       "#f3b164", "#aa6d37")
+woodworkR_palette_names <- c("ebony", "wenge", "walnut",
+                             "whiteoak", "pine", "cedar", "mahogany", 
+                             "beech", "teak", "redwood")
+woodworkR_palette <- c("#2c3227", "#645452", "#6c3f19", 
+                       "#ce9f6f", "#deccba", "#a1655b", "#c04000", 
+                       "#f3b164", "#aa6d37", "#5F3730")
 
 test %>% 
           ggplot(aes(x, y, color = factor(y))) +
